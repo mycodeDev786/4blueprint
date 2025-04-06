96da17743c86f3a29995aeb356db1a54b3e642fb
villageprofessor <113996275+mycodeDev786@users.noreply.github.com>
2025-04-06 22:33:59 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,12 @@ const nextConfig = {
   //: "export",  // <=== enables static exports
   //reactStrictMode: true,
   images: {
-    domains: ["flagcdn.com", "randomuser.me", "localhost"], // Allow external images from flagcdn.com
+    domains: [
+      "flagcdn.com",
+      "randomuser.me",
+      "localhost",
+      "4blueprint.duckdns.org",
+    ], // Allow external images from flagcdn.com
   },
 };
 
